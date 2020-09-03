0bbbefc0fdd3f542f60322aad3bb1e47ebc6b5b6
toge-piiiiij <jia0.togepiii@gmail.com>
2020-09-03 15:05:28 +0800

    Updates

---
+++ precache-manifest.ca09d57c5f46678cd6838f0fd2617359.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953c4c2b7d561ab0a23f46b70f4a93a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "371af3609fbffe063117",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4d58d79f94ecf3263bba",
+    "url": "/static/js/2.4b1e88d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4b1e88d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "371af3609fbffe063117",
+    "url": "/static/js/main.8b749d92.chunk.js"
+  },
+  {
+    "revision": "d6a594a7c11a236ff335",
+    "url": "/static/js/runtime-main.e88a0095.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
